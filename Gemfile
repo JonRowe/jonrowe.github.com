85bbf003ba9bf6699a095ce29d3c0ec5344e8da6
Jon Rowe <hello@jonrowe.co.uk>
2013-01-26 22:50:16 +1100

    stylesheet bundle setup

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source :rubygems
 
 gem 'middleman'
 gem 'middleman-smusher'
+gem 'middleman-sprockets'
 gem 'middleman-gh-pages'
--- config.rb
+++ config.rb
@@ -2,6 +2,8 @@ set :css_dir,    'stylesheets'
 set :js_dir,     'javascripts'
 set :images_dir, 'images'
 
+activate :sprockets
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
